6494609fe24e80c4b97d9f986e2308cd6e408b61
jose cardona <josesiprozmaster@gmail.com>
2023-06-28 22:48:08 -0500

    new update portfolio 1.1

--- js/script__model.js
+++ js/script__model.js
@@ -1,29 +1,127 @@
+
+//Clase para el boton de modo oscuro - dark switch
 export class switchs{
 
     constructor(){
 
     }
 
-    //Función para el switch
+    //Método para el switch
 
-    colocarClass(switchTocado, body){
+    colocarClass(switchTocado, body,modal,txtModal){
+
+        const modalSwitch = modal;
 
         switchTocado.classList.toggle('active');
         body.classList.toggle('content__dark');
+        modalSwitch.classList.toggle('active');
+
+        txtModal.innerHTML = "Ha activado el modo oscuro";
+
 
     }
 
 }
+
+//Clase para el menuHaburguesa
 
 export class menuHamburger{
     constructor(){
 
     }
 
+    //Método para el click del menú hamburguesa
     menuHamburger(contentMenu){
         const contentMenuHamburger = contentMenu;
 
         contentMenuHamburger.classList.toggle('active');
+
+    }
+}
+
+
+//Classe para el mensaje al momento de click de la imagen personal
+export class btnImagePerson{
+    constructor(){
+
+    }
+
+    //Método para btnImage personal
+    btnImageinformacion(modal__person, txtModal){
+        const contentMessage = modal__person;
+        contentMessage.classList.toggle('active');
+
+        txtModal.innerHTML = `¡Hola! <br>¿Te ayudo en algo? Contáctame`;
+    }
+}
+
+//Se indica el tipo de error a mostrar
+
+const tipoDeError = [
+    'valueMissing',
+    'typeMismatch'
+]
+
+//Se indica los errores a mostrar dependiendo del tipo de input
+const mensajeErrores = {
+    nombre: {
+        valueMissing: "El campo nombre no ha sido definido",
+    },
+    correo: {
+        valueMissing: "El campo correo no ha sido definido",
+        typeMismatch: "El correo ingresado no es permirido",
+    },
+    asunto: {
+        valueMissing: "El campo asunto no ha sido definido",
+    },
+    mensaje: {
+        valueMissing: "El campo mensaje no ha sigo definido"
+    }
+
+}
+
+//Clase para la validacion del formulario contacto
+
+export class validacionFormulario{
+
+    constructor(){
+    }
+
+
+    //Método para validar el input
+    validarForm(input){
+
+        const tipoDeInput = input.dataset.tipo;
+        let span = input.parentElement.querySelector('.form__message--error');
+
+
+        if(input.validity.valid){
+            input.classList.remove('error');
+            input.parentElement.querySelector('.form__message--error').innerHTML = '';
+        }else{
+
+            input.classList.add('error');
+            span.classList.add('error');
+            span.innerHTML = this.mostrarMensajesErrores(tipoDeInput,input);
+
+        }
+
+    }
+
+    mostrarMensajesErrores(tipoDeInput,input){
+
+        let mensaje = '';
+
+        tipoDeError.forEach(error =>{
+
+            if(input.validity[error]){
+                mensaje = mensajeErrores[tipoDeInput][error]
+            }
+        })
+
+        return mensaje;
+        
+        
 
     }
 }
--- js/script_controller.js
+++ js/script_controller.js
@@ -1,6 +1,8 @@
 
 import { switchs } from "./script__model.js";
 import { menuHamburger } from "./script__model.js";
+import { btnImagePerson } from "./script__model.js";
+import { validacionFormulario } from "./script__model.js";
 
 
 //Botón para el modo oscuro o dark
@@ -11,17 +13,33 @@ const body      = document.getElementById('body');
 const contentMenu  = document.getElementById('header__content--nav');
 const btnHamburger = document.getElementById('header__hamburger');
 
+//click de la imagen persona
+const btnImage    = document.getElementById('right__image--person');
+const modal__person = document.getElementById('modal__image--person');
+const txtModal      = document.getElementById('txtModal--image');
+
+
+//inputs del formulario contacto
+const inputs = document.querySelectorAll('.form__input');
+const txtSpan = document.querySelector('form__message--error');
+
 //Instanciar la clase para el switch
 const swtichTocado = new switchs();
 
 //Instanciar la clase para el menuHamburger
 const btnHamburgerClass = new menuHamburger();
 
+//Instanciar la clase para el mensaje de la imagen personal
+const btnImageClass    = new btnImagePerson();
+
+//Instanciar la clase para la validación del formulario
+const validacionFormul  = new validacionFormulario();
+
 
 //Escuchar el evento click del switch - modo dark
 btnSwitch.addEventListener('click', ()=>{
 
-    swtichTocado.colocarClass(btnSwitch, body)
+    swtichTocado.colocarClass(btnSwitch, body, modal__person,txtModal)
 
 });
 
@@ -29,5 +47,21 @@ btnSwitch.addEventListener('click', ()=>{
 btnHamburger.addEventListener('click',()=>{
 
     btnHamburgerClass.menuHamburger(contentMenu);
-    
+})
+
+//Escuchaar el evento click del btnImge
+btnImage.addEventListener('click', ()=>{
+
+    btnImageClass.btnImageinformacion(modal__person,txtModal);
+
+})
+
+//Recorrer la lista de los inputs y agregar el evento blur de cada elemento input
+//Enviar como parametro el evento o input element
+
+inputs.forEach((input)=>{
+    input.addEventListener('blur', (event)=>{
+        validacionFormul.validarForm(event.target);
+        console.log(input)
+    })
 })
